f3417c7fe0556827c2d5189735222662a9e6a1fc
churitos44 <52306785+churitos44@users.noreply.github.com>
2020-06-24 17:21:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+          static void Main(string[] args)
+        {
+             if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            
+            }
+
+            Console.WriteLine("Ingresa tu nombre por favor :");
+            string nombre = Console.ReadLine();
+           
+            Console.WriteLine("Realiza tu suma");
+            int primerNumero, segundoNumero, suma;
+
+            Console.Write("Introduce el primer número: ");
+            primerNumero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduce el segundo número: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+            suma = primerNumero + segundoNumero;
+
+            Console.WriteLine(" Hola {0} La suma de {1} y {2} es {3}",
+                nombre ,primerNumero, segundoNumero, suma);
+        }
+    }
+}
